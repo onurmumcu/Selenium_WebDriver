198f5bc50ed63bd32d166aa23ff3d26cf1d40cde
onurmumcu <onur.mmc@gmail.com>
2019-07-27 18:03:11 -0400

    gggg

--- src/test/java/AutoIT.java
+++ src/test/java/AutoIT.java
@@ -1,9 +1,11 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
@@ -17,13 +19,23 @@ public class AutoIT {
 	@BeforeClass
 	public void setUp() {
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-		driver = new ChromeDriver();
+		
 	}
 	
 	@Test
 	public void deneme() throws IOException, InterruptedException {
 		String downloadPath = System.getProperty("user.dir");
 		System.out.println(downloadPath);
+		
+		
+		HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+
+		chromePrefs.put("profile.default_content_settings.popups", 0);
+
+		chromePrefs.put("download.default_directory", downloadPath);
+		ChromeOptions option =new ChromeOptions();
+		option.setExperimentalOption("prefs", chromePrefs);
+		driver = new ChromeDriver(option);
 		driver.get("https://altoconvertpdftojpg.com/");
 		driver.findElement(By.cssSelector("[class*='btn--choose']")).click();
 		Thread.sleep(3000);
@@ -36,7 +48,9 @@ public class AutoIT {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Download Now")));
 		driver.findElement(By.linkText("Download Now")).click();
 		
-		File f =new File("C:\\Users\\Onur\\Downloads\\Converted.zip");
+		Thread.sleep(3000);
+		
+		File f =new File(downloadPath+"\\Converted.zip");
 		if(f.exists()) {
 			System.out.println("Dosya yuklenmis   :) ");
 		}else {
